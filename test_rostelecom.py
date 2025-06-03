a05a4e76d06255fea821452f333daf098067766e
Boris <borman-dallas@yandex.ru>
2025-06-03 14:44:45 +0400

    Update files

--- test_rostelecom.py
+++ test_rostelecom.py
@@ -16,8 +16,9 @@ def driver():
     driver.quit()
 
 def test_authorization_to_email_valid(driver):
+    """Tests login by email and password with valid data."""
     # Долгая загрузка страницы, ожидаем ее загрузки
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     # Вводим валидный лоигн и пароль
     driver.find_element(By.ID, 'username').send_keys('boristester@yandex.ru')
     driver.find_element(By.ID, 'password').send_keys('6W2-ssC-3z2-c8V')
@@ -28,8 +29,9 @@ def test_authorization_to_email_valid(driver):
 
 
 def test_authorization_to_email_password_not_valid(driver):
+    """Tests login by email and password with incorrect password"""
     # Долгая загрузка страницы, ожидаем ее загрузки
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     # Вводим валидный лоигн и пароль
     driver.find_element(By.ID, 'username').send_keys('boristester@yandex.ru')
     driver.find_element(By.ID, 'password').send_keys('6W2-ssC-3z2-c8Vq')
@@ -39,8 +41,9 @@ def test_authorization_to_email_password_not_valid(driver):
     assert driver.find_element(By.ID, 'form-error-message').text == "Неверный логин или пароль"
 
 def test_authorization_to_email_login_not_valid(driver):
+    """Tests login by email and password with incorrect login"""
     # Долгая загрузка страницы, ожидаем ее загрузки
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     # Вводим валидный лоигн и пароль
     driver.find_element(By.ID, 'username').send_keys('boristester@yndex.ru')
     driver.find_element(By.ID, 'password').send_keys('6W2-ssC-3z2-c8Vq')
@@ -48,3 +51,27 @@ def test_authorization_to_email_login_not_valid(driver):
     #driver.implicitly_wait(10)
     # Проверяем прошла ли авторизация и открылась страница имеено введенного аккаунта
     assert driver.find_element(By.ID, 'form-error-message').text == "Неверный логин или пароль"
+
+def test_open_page_registration(driver):
+    """Tests whether the registration page opens"""
+    driver.implicitly_wait(5)
+    driver.find_element(By.ID, 'kc-register').click()
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "Регистрация"
+
+def test_registration_without_region(driver):
+    """Tests registration with the fields entered except for the Region field."""
+    driver.implicitly_wait(5)
+    driver.find_element(By.ID, 'kc-register').click()
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "Регистрация"
+    driver.find_element(By.NAME, 'firstName').send_keys('Федор')
+    driver.find_element(By.NAME, 'lastName').send_keys('Двинятин')
+    driver.find_element(By.ID, 'address').send_keys('bo@ya.ru')
+    driver.find_element(By.ID, 'password').send_keys('Aa12345678')
+    driver.find_element(By.ID, 'password-confirm').send_keys('Aa12345678')
+    driver.find_element(By.NAME, 'register').click()
+    assert driver.find_element(By.XPATH, '//span[contains(text(), "Укажите регион")]')
+
+def test_password_recovery_page(driver):
+
+    driver.find_element(By.ID, 'forgot_password').click()
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "Восстановление пароля"
