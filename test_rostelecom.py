3c3b3cdd1400d7078eaa8032ec3d8562890b73e3
Boris <borman-dallas@yandex.ru>
2025-06-03 11:31:01 +0400

    Update test_rostelecom.py

---
+++ test_rostelecom.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(autouse=True)
+def driver():
+    driver = webdriver.Chrome()
+    # Переходим на страницу авторизации
+    driver.get('https://petfriends.skillfactory.ru/login')
+
+    yield driver
+
+    driver.quit()
